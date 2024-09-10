38d9905c2ed1c07c56be3c1b80146272f20e5bc2
simony05 <67559452+simony05@users.noreply.github.com>
2024-09-09 21:34:47 -0700

    videos working

--- script.js
+++ script.js
@@ -5,22 +5,71 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-function openModalVideo(modalId) {
-    const modalVideoContainer = document.getElementById(modalId);
-    modalVideoContainer.style.display = 'block';
+const buttonValues = {
+  video1: '5BdGLMO1mQs?si=zcw5iwbM5oDumj4-&amp;start=158',
+  video2: 'H6BRfTwE9zc?si=GgK8jFPvEItorMdM',
+  video3: 'Value 3',
+};
+
+const modalBtns = document.querySelectorAll('.modal-btn');
+const modal = document.querySelector('.modal-video-container');
+
+modalBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const buttonId = e.target.id;
+    const buttonValue = buttonValues[buttonId];
+    modal.innerHTML = `
+      <div class="modal-video">
+        <iframe 
+          width="560" 
+          height="315" 
+          src="https://www.youtube.com/embed/${buttonValue}" 
+          title="YouTube video player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerpolicy="strict-origin-when-cross-origin" 
+          allowfullscreen>
+        </iframe>
+      </div>
+    `;
+    modal.style.display = "flex";
+    setTimeout(() => {
+      modal.style.opacity = "1";
+    }, 50);
+  });
+});
+
+modal.addEventListener('click', () => {
+  modal.innerHTML = '';
+  modal.style.opacity = "0";
+  setTimeout(() => {
+    modal.style.display = "none";
+  }, 450);
+});
+
+const modalVideo = document.querySelector('.modal-video');
+
+modalVideo.addEventListener('load', () => {
+  modalVideo.classList.add('show');
+});
+
+const showModalButton = document.querySelector('#show-modal-button');
+showModalButton.addEventListener('click', () => {
+  modalVideo.classList.add('show');
+});
+
+document.querySelectorAll('.modal-video').forEach((modalVideo) => {
+  modalVideo.addEventListener('click', (e) => {
+    if (e.target === modalVideo) {
+      modalVideo.style.display = 'none';
+    }
+  });
+});
+
+const modalContainer = document.querySelector('.modal-container');
+
+modalContainer.addEventListener('click', (e) => {
+  if (e.target === modalContainer) {
+    modalContainer.style.display = 'none';
   }
-  
-  document.querySelectorAll('.close-btn').forEach((closeBtn) => {
-    closeBtn.addEventListener('click', () => {
-      const modalVideoContainer = closeBtn.parentNode.parentNode;
-      modalVideoContainer.style.display = 'none';
-    });
-  });
-  
-  document.querySelectorAll('.modal-video-container').forEach((modalVideoContainer) => {
-    modalVideoContainer.addEventListener('click', (e) => {
-      if (e.target === modalVideoContainer) {
-        modalVideoContainer.style.display = 'none';
-      }
-    });
-  });
+});
